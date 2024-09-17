a5113a2100e9fb1c9fbdc0caadf0f9597d03d6fe
PhoeNYxcash <143548758+PhoeNYxcash@users.noreply.github.com>
2024-09-17 13:26:50 -0400

    Time: 13 ms (70.92%), Space: 11.6 MB (73%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -8,18 +8,9 @@ class Solution(object):
         l1 = s1.split(" ")
         l2 = s2.split(" ")
         ret = []
-        p = {}
-        for n in l1:
-            if n not in p:
-                p[n] = 1
-            else: 
-                p[n] += 1
-        for n in l2:
-            if n not in p:
-                p[n] = 1
-            else: 
-                p[n] += 1
-        for k,v in p.items():
+        freq = Counter(l1 + l2)
+        print(freq)
+        for k,v in freq.items():
             if v == 1:
                 ret.append(k)
 
